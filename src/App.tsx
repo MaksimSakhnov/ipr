6d6f53499bb4344c3783ee446d6d5c0d92c25d76
Максим Сахнов <maxim.sahnov@satellite-soft.ru>
2024-02-12 17:47:24 +0400

    Добавлены react-laze

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { lazy, Suspense } from 'react';
 import { ThemeProvider } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import AppHeaderContainer from 'containers/AppHeaderContainer';
-import PageFilms from 'pages/PageFilms';
-import PagePeople from 'pages/PagePeople';
 import { DarkTheme, LightTheme } from 'shared/themes/Themes.const';
 import { useAppStore } from 'store';
 import { FILMS, MAIN } from 'routes/routes';
+
+const PageFilms = lazy(() => import('pages/PageFilms'));
+const PagePeople = lazy(() => import('pages/PagePeople'));
 
 function App() {
   const darkTheme = useAppStore((state) => state.darkTheme);
@@ -15,10 +17,12 @@ function App() {
       <AppHeaderContainer />
 
       <div>
-        <Routes>
-          <Route path={MAIN} element={<PagePeople />} />
-          <Route path={FILMS} element={<PageFilms />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path={MAIN} element={<PagePeople />} />
+            <Route path={FILMS} element={<PageFilms />} />
+          </Routes>
+        </Suspense>
       </div>
     </ThemeProvider>
   );
